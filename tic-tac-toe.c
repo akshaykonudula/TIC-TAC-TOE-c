0e544b12fa09ad61269768fe1d56f82af068fb21
akshaykonudula <86998803+akshaykonudula@users.noreply.github.com>
2025-10-06 16:24:14 +0530

    Update tic-tac-toe.c

--- tic-tac-toe.c
+++ tic-tac-toe.c
@@ -1,14 +1,1 @@
-# Tic Tac Toe in C
 
-A simple command-line Tic Tac Toe game implemented in C.
-
-## Features
-- Two-player gameplay
-- Input validation
-- Win detection
-- Tie game detection
-- Play again option
-
-## How TO COMPILE AND RUN
-gcc tictactoe.c -o tictactoe
-./tictactoe
